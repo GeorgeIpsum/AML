2bc78c395c7b71764cd76c460489c3f92b4ec846
Ibrahim Saberi <georgeipsum@gmail.com>
2019-10-08 16:38:33 -0400

    Updates

---
+++ precache-manifest.7c9185bda75a6862c5f31379d9e89950.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8000b4015400f9d09869aebee1f4dad1",
+    "url": "/fruition/index.html"
+  },
+  {
+    "revision": "8406a4664cc6c9b6e3e5",
+    "url": "/fruition/static/css/main.378a105e.chunk.css"
+  },
+  {
+    "revision": "0f25b5c2a739fc5266b1",
+    "url": "/fruition/static/js/2.257d3c31.chunk.js"
+  },
+  {
+    "revision": "8406a4664cc6c9b6e3e5",
+    "url": "/fruition/static/js/main.6f60cd9d.chunk.js"
+  },
+  {
+    "revision": "85184502eb5e218add2f",
+    "url": "/fruition/static/js/runtime~main.c2438661.js"
+  }
+]);
