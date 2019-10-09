35dc1f0945627e0bec0ebf1d10103473667e519f
Ibrahim Saberi <georgeipsum@gmail.com>
2019-10-09 18:45:39 -0400

    context selection and new contexts

--- src/components/context-select/context-select.tsx
+++ src/components/context-select/context-select.tsx
@@ -17,23 +17,56 @@ export default class ContextSelect extends React.Component<{contextStore: any}, 
     };
   }
 
+  changeSelectedContext = (event: any) => {
+    const id = event.target.id;
+    this.setState({selectedContext: this.props.contextStore.findById(id)});
+    this.props.contextStore.setDefaultContext(id);
+  }
+
+  onFormSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { newContextInput } = this.state;
+    const { contextStore } = this.props;
+    if(newContextInput.length !== 0 && !contextStore.findByName(newContextInput)) {
+      contextStore.addContext(newContextInput);
+    }
+  }
+
+  onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({newContextInput: event.target.value});
+  }
+
   toggleOpen = () => {
     this.setState(state => ({isOpen: !state.isOpen}));
   }
 
   render() {
-    const { isOpen, selectedContext } = this.state;
+    const { isOpen, selectedContext, newContextInput } = this.state;
+    const { contextStore } = this.props;
+    const contexts = contextStore.contexts.slice().filter((c) => c.id !== selectedContext.id).map(
+      (c) => (<div key={c.id} id={c.id} className="context" onClick={this.changeSelectedContext}>{ c.name }</div>)
+    );
+    const isDisabled = newContextInput.length === 0 || contextStore.findByName(newContextInput);
     return(
       <div className="Context">
-        <div className="current-context" onClick={this.toggleOpen} style={{borderRadius: isOpen ? '4px 4px 0' : ''}}>
+        <div className="title">CONTEXT</div>
+        <div className="current-context" onClick={this.toggleOpen} style={{borderRadius: isOpen ? '4px 4px 0 0' : ''}}>
           <div className="context">{ selectedContext.name }</div>
           <div className="arrow">
             <div className="arrow-inner"></div>
           </div>
         </div>
-        <div className="title">context</div>
         <div className="other-contexts" style={{display: isOpen ? 'block' : 'none'}}>
-          <PlusCircle size={16} />
+          { contexts }
+          <div className="add-context">
+            <form onSubmit={this.onFormSubmit}>
+              <label htmlFor="addNewContext">ADD CONTEXT</label>
+              <input id="addNewContext" type="text" value={newContextInput} onChange={this.onInputChange} />
+              <button type="submit" disabled={isDisabled}>
+                <PlusCircle size={16} />
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     );
