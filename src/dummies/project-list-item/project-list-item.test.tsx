0c4938f889be4aff998178aabaf0507a577e8467
Ibrahim Saberi <georgeipsum@gmail.com>
2019-10-19 02:32:58 -0400

    unit tests

--- src/dummies/project-list-item/project-list-item.test.tsx
+++ src/dummies/project-list-item/project-list-item.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ProjectListItem from './project-list-item';
+import { ProjectModel } from '../../models/project';
+
+const handleCallback = () => {
+  return true;
+}
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ProjectListItem />, div);
+  const project = ProjectModel.create();
+  ReactDOM.render(<ProjectListItem project={project} onGenericCallback={handleCallback} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
