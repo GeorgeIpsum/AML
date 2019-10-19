d6732d6e3e6e0000a663ea6d70e02b3ddc8b80d0
Ibrahim Saberi <georgeipsum@gmail.com>
2019-10-19 15:20:45 -0400

    added fallback environment variables incase process.env isn't available (primarily for jest)

---
+++ env.test.ts
@@ -0,0 +1,7 @@
+export const REACT_APP_API_KEY='';
+export const REACT_APP_AUTH_DOMAIN='*.firebaseapp.com';
+export const REACT_APP_DATABASE_URL='https://*.firebaseio.com';
+export const REACT_APP_PROJECT_ID='';
+export const REACT_APP_STORAGE_BUCKET='';
+export const REACT_APP_MESSAGING_SENDER_ID='';
+export const REACT_APP_APP_ID='';
